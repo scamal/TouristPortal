76aa8d06f37e6ff3b7e80ae7e85b0d3bc44a0c25
Gregor <lulic.gregor.gregor@gmail.com>
2019-12-16 22:35:13 +0100

    izgled gornjeg dela svake stranice

--- index.php
+++ index.php
@@ -4,18 +4,62 @@
     <title>Bootstrap Scrollspy 1</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--font-->
+    <link href="https://fonts.googleapis.com/css?family=Didact+Gothic&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link type="text/css" rel="stylesheet" href="style.css">
 
 </head>
 <body>
-<div class="container">
-    <div class="row">
-        <div class="col-12 title">
-<?php
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#section1">Section 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#section2">Section 2</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#section3">Section 3</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                    Section 4
+                </a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#section41">Link 1</a>
+                    <a class="dropdown-item" href="#section42">Link 2</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <?php
+    session_start();
+    if (!isset($_SESSION['logged'])){
+        echo "<div>
+        <a href='logIn.php' class='btn btn-primary'>Log in</a>
+        
+        <a href='signUp.php' class='btn btn-primary'>Sign Up</a>
+        
+        </div> ";
+    }
 
-?>
+    ?>
+</nav>
+<div class="container-fluid">
+
+    <div class="row title align-items-center">
+
+        <div class="col-12 ">
+            <h1 class="title-font white-text">DOBRODOŠLI U BEOGRAD</h1>
+
         </div>
     </div>
 </div>
